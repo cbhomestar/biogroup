e9583e002404d51bde8e866757eb5d638637a17a
Kaleb Olson <kaleb.olson@gmail.com>
2016-09-08 15:55:51 -0600

    added fasta processing and frequentword search

--- ecoli.py
+++ ecoli.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
 import prob_ba1f as p
+import prob_ba1j as j
 
 ecoli_file = "data/ecoli.fasta"
 with open(ecoli_file, 'r') as f:
     metadata = f.readline()
-    genome = f.read()
+    genome = f.read().replace('\n','')
 
 
 print(metadata)
 print(genome[:50])
-print(p.min_skew_points(genome))
+mins = p.min_skew_points(genome)
+
+print(mins)
+subgenome = genome[mins[0] - 500: mins[0] + 500]
+result = j.faster_frequent_words(subgenome, 9, distance=1)
+print(result)
