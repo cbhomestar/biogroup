39dd31f6f9f6b19a4dc442d1fe831e970477188c
SazedHarmony <ryanrupper@yahoo.com>
2016-10-15 17:07:10 -0600

    Update ssrfinder.py

--- ssrfinder.py
+++ ssrfinder.py
@@ -1,1 +1,22 @@
 #Put your code here
+import sys
+
+for i in (range(1, len(sys.argv))):
+           with open(sys.argv[i]) as file:
+                sequences = {}
+                label = ""
+                sequence = ""
+                sequenceNumber = 0
+                for line in file:
+                  line = line.strip()
+                  if (line[0] == ">"):
+                    if (sequenceNumber > 0):
+                      sequences[label] = sequence
+                      label = line[1:]
+                      sequence = ""
+                      sequenceNumber += 1
+                    else:
+                      label = line[1:]
+                      sequenceNumber += 1
+                  else:
+                    sequence += line
