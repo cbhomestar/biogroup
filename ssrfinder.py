8a65e5928721b15fc2599b15ebaf3620debcdfee
SazedHarmony <ryanrupper@yahoo.com>
2016-11-17 22:32:14 -0700

    Update ssrfinder.py

--- ssrfinder.py
+++ ssrfinder.py
@@ -1,26 +1,29 @@
+import re
 import sys
 
 class Organism(object):
-	def __init__(name, file):
+	def __init__(self, name, file):
 		self.name = name
 		self.file = file
 		self.ssrs = []
 
-	def addSSR(ssr):
+	def addSSR(self, ssr):
 		self.ssrs.append(ssr)
 
-	def getListOfSSRs():
+	def getListOfSSRs(self):
 		return self.ssrs
 
 class SSR:
-	pattern
-	repeatNum=0
-	totalSize
-	contigName
-	startLocation
-	endLocation
-	left50
-	right50
+	pattern = ""
+	repeatNum = 0
+	totalSize = 0
+	contigName = ""
+	startLocation = 0
+	endLocation = 0
+	left50 = ""
+	right50 = ""
+	label = ""
+	number = 0
 	def _init_(self, pattern):
 		self.pattern=pattern
 		totalSize=len(pattern) #i dont know if this is right
@@ -28,6 +31,14 @@ class SSR:
 		self.pattern=pattern
 	def getPattern(self):
 		return self.pattern
+	def setNumber(self,Num):
+		self.number=Num
+	def getNumber(self):
+		return self.number
+	def setLabel(self,label):
+		self.label=label
+	def getLabel(self):
+		return self.label
 	def setRepeatNum(self,num):
 		self.repeatNum=num
 	def addToRepeatNum(self,num):
@@ -60,8 +71,6 @@ class SSR:
 		return self.right50
 	
 def calculatePossibleSSRs(myNum):
-	if not myNum.isdigit():
-		print "ERROR: You need to insert a digit for max kmer size"
 	myKmers=list()
 	nucleotides=('A','T','G','C')
 	for i in range(2,int(myNum)+1):
@@ -80,7 +89,6 @@ def calculatePossibleSSRs(myNum):
 			if len(result)%2==0:
 				if result[0:len(result)/2]!=result[len(result)/2:len(result)]:
 					myKmers.append(result)
-					print result
 			else:
 				ok=False
 				start=result[0]
@@ -92,6 +100,8 @@ def calculatePossibleSSRs(myNum):
 	return myKmers
 
 def compareSeqs(s1, s2):
+	if (len(s1) <= 7) or (len(s2) <= 7):
+		return False
 	s1.upper()
 	s2.upper()
 	matrix = [[0 for x in range(len(s2) + 1)] for y in range(len(s1) + 1)]
@@ -117,7 +127,7 @@ def compareSeqs(s1, s2):
 			matrix[i][j] = max(score1, score2, score3)
 			if matrix[i][j] > maxscore:
 				maxscore = matrix[i][j]
-	if maxscore >= (.85 * min(len(s1), len(s2)):
+	if maxscore >= (.85 * min(len(s1), len(s2))):
 		return True
 	return False
 
@@ -130,6 +140,22 @@ def getPatternFromSSR(ssr):
 	repeatNumber = {2: 8, 3: 6, 4: 5, 5: 4}
 	pattern = ssr * repeatNumber[len(ssr)]
 	return pattern
+
+def reverseComplement(seq):
+	output = ""
+	seq = seq[::-1]
+	for letter in seq:
+		if letter == 'A':
+			output += "T"
+		if letter == 'C':
+			output += "G"
+		if letter == 'G':
+			output += "C"
+		if letter == 'T':
+			output += "A"
+		if letter == 'N':
+			output += "N"
+	return output
 			
 organismList = []
 for i in (range(1, len(sys.argv))):
@@ -155,14 +181,75 @@ for i in (range(1, len(sys.argv))):
 			else:
                 		sequence += line
 		kmers = []
+		ssrNumber = 1
 		for j in range(2, 6):
 			kmers = calculatePossibleSSRs(j)
 			for kmer in kmers:
 				for label in sequences:
-					foundSSRS = findSSRs(sequences[label], kmer)
+					sequence = sequences[label]
+					foundSSRS = findSSRs(sequence, kmer)
 					for index in foundSSRS:
-						newSSR = SSR(kmer)
+						newSSR = SSR()
+						newSSR.setLabel(label)
+						newSSR.setPattern(kmer)
+						newSSR.setNumber(ssrNumber)
+						ssrNumber += 1
 						newSSR.setContigName(label)
-						newSSR.setStartLocation(index)
+						newSSR.setStartLocation(index[0])
+						k = 0
+						while sequence[(index[0] + k * len(kmer)): (index[0] + k * len(kmer) + len(kmer))] == kmer:
+							k += len(kmer)
+						newSSR.setRepeatNum(k)
+						newSSR.setEndLocation(index[0] + k * len(kmer) + 1)
+						newSSR.setTotalSize(k * len(kmer))
+						if (index[0] < 50):
+							newSSR.setLeft50(sequence[0:(index[0])])
+						else:
+							newSSR.setLeft50(sequence[(index[0] - 50): (index[0])])
+						if (newSSR.getEndLocation() + 50) > len(sequence):
+							newSSR.setRight50(sequence[newSSR.getEndLocation():])
+						else:
+							newSSR.setRight50(sequence[newSSR.getEndLocation():newSSR.getEndLocation() + 50]) 
+						organism.addSSR(newSSR)
 		organismList.append(organism)
+if (len(organismList) == 2):
+	outFile = open("output.csv", 'w')
+	outFile.write("Sample,Label,SSR #,SSR type,SSR,size,Start,End,Left 50,Right 50\n")
+	ssrList1 = organismList[0].getListOfSSRs()
+	ssrList2 = organismList[1].getListOfSSRs()
+	for i in range(len(ssrList1)):
+		ssr1 = ssrList1[i]
+		for j in range(len(ssrList2)):
+			ssr2 = ssrList2[j]
+			if (ssr1.getPattern() == ssr2.getPattern() and ssr1.getRepeatNum() != ssr2.getRepeatNum() and compareSeqs(ssr1.getLeft50(), ssr2.getLeft50()) and compareSeqs(ssr1.getRight50(), ssr2.getRight50())):
+				outFile.write("1," + ssr1.getLabel() + "," + str(ssr1.getNumber()) + ",p" + str(len(ssr1.getPattern())) + ",(" + ssr1.getPattern() + ")" + str(ssr1.getTotalSize() / len(ssr1.getPattern())) + "," + str(ssr1.getTotalSize()) + "," + str(ssr1.getStartLocation()) + "," + str(ssr1.getEndLocation()) + "," + ssr1.getLeft50() + "," + ssr1.getRight50() + '\n')
+				outFile.write("2," + ssr2.getLabel() + "," + str(ssr2.getNumber()) + ",p" + str(len(ssr2.getPattern())) + ",(" + ssr2.getPattern() + ")" + str(ssr2.getTotalSize() / len(ssr2.getPattern())) + "," + str(ssr2.getTotalSize()) + "," + str(ssr2.getStartLocation()) + "," + str(ssr2.getEndLocation()) + "," + ssr2.getLeft50() + "," + ssr2.getRight50() + '\n')
+			if (ssr1.getPattern() == reverseComplement(ssr2.getPattern()) and ssr1.getRepeatNum() != ssr2.getRepeatNum() and compareSeqs(ssr1.getLeft50(), reverseComplement(ssr2.getRight50())) and compareSeqs(ssr1.getRight50(), reverseComplement(ssr2.getLeft50()))):
+				outFile.write("1," + ssr1.getLabel() + "," + str(ssr1.getNumber()) + ",p" + str(len(ssr1.getPattern())) + ",(" + ssr1.getPattern() + ")" + str(ssr1.getTotalSize() / len(ssr1.getPattern())) + "," + str(ssr1.getTotalSize()) + "," + str(ssr1.getStartLocation()) + "," + str(ssr1.getEndLocation()) + "," + ssr1.getLeft50() + "," + ssr1.getRight50() + '\n')
+				outFile.write("2," + ssr2.getLabel() + "," + str(ssr2.getNumber()) + ",p" + str(len(ssr2.getPattern())) + ",(" + ssr2.getPattern() + ")" + str(ssr2.getTotalSize() / len(ssr2.getPattern())) + "," + str(ssr2.getTotalSize()) + "," + str(ssr2.getStartLocation()) + "," + str(ssr2.getEndLocation()) + "," + ssr2.getLeft50() + "," + ssr2.getRight50() + '\n')
+	outFile.close()
+if (len(organismList) > 2):
+	outFile = open("comparisonOutput.csv", 'w')
+	outFile.write("Sample,Label,SSR #,SSR type,SSR,size,Start,End,Left 50,Right 50\n")
+	ssrList1 = organismList[0].getListOfSSRs()
+	ssrList2 = organismList[1].getListOfSSRs()
+	for i in range(len(ssrList1)):
+		ssr1 = ssrList1[i]
+		for j in range(len(ssrList2)):
+			ssr2 = ssrList2[j]
+			if (ssr1.getPattern() == ssr2.getPattern() and ssr1.getRepeatNum() != ssr2.getRepeatNum() and compareSeqs(ssr1.getLeft50(), ssr2.getLeft50()) and compareSeqs(ssr1.getRight50(), ssr2.getRight50())):
+				outFile.write("1," + ssr1.getLabel() + "," + str(ssr1.getNumber()) + ",p" + str(len(ssr1.getPattern())) + ",(" + ssr1.getPattern() + ")" + str(ssr1.getTotalSize() / len(ssr1.getPattern())) + "," + str(ssr1.getTotalSize()) + "," + str(ssr1.getStartLocation()) + "," + str(ssr1.getEndLocation()) + "," + ssr1.getLeft50() + "," + ssr1.getRight50() + '\n')
+				outFile.write("2," + ssr2.getLabel() + "," + str(ssr2.getNumber()) + ",p" + str(len(ssr2.getPattern())) + ",(" + ssr2.getPattern() + ")" + str(ssr2.getTotalSize() / len(ssr2.getPattern())) + "," + str(ssr2.getTotalSize()) + "," + str(ssr2.getStartLocation()) + "," + str(ssr2.getEndLocation()) + "," + ssr2.getLeft50() + "," + ssr2.getRight50() + '\n')
+			for k in range(2, len(organismList)):
+				childSSRs = organismList[k].getListOfSSRs()
+				for l in range(len(childSSRs)):
+					ssrC = childSSRs[l]
+					if (ssr1.getPattern() == ssrC.getPattern() and compareSeqs(ssr1.getLeft50(), ssrC.getLeft50()) and compareSeqs(ssr1.getRight50(), ssrC.getRight50())):
+						outFile.write("1," + ssrC.getLabel() + "," + str(ssrC.getNumber()) + ",p" + str(len(ssrC.getPattern())) + ",(" + ssrC.getPattern() + ")" + str(ssrC.getTotalSize() / len(ssrC.getPattern())) + "," + str(ssrC.getTotalSize()) + "," + str(ssrC.getStartLocation()) + "," + str(ssrC.getEndLocation()) + "," + ssrC.getLeft50() + "," + ssrC.getRight50() + '\n')
+
+			if (ssr1.getPattern() == reverseComplement(ssr2.getPattern()) and ssr1.getRepeatNum() != ssr2.getRepeatNum() and compareSeqs(ssr1.getLeft50(), reverseComplement(ssr2.getRight50())) and compareSeqs(ssr1.getRight50(), reverseComplement(ssr2.getLeft50()))):
+				outFile.write("1," + ssr1.getLabel() + "," + str(ssr1.getNumber()) + ",p" + str(len(ssr1.getPattern())) + ",(" + ssr1.getPattern() + ")" + str(ssr1.getTotalSize() / len(ssr1.getPattern())) + "," + str(ssr1.getTotalSize()) + "," + str(ssr1.getStartLocation()) + "," + str(ssr1.getEndLocation()) + "," + ssr1.getLeft50() + "," + ssr1.getRight50() + '\n')
+				outFile.write("2," + ssr2.getLabel() + "," + str(ssr2.getNumber()) + ",p" + str(len(ssr2.getPattern())) + ",(" + ssr2.getPattern() + ")" + str(ssr2.getTotalSize() / len(ssr2.getPattern())) + "," + str(ssr2.getTotalSize()) + "," + str(ssr2.getStartLocation()) + "," + str(ssr2.getEndLocation()) + "," + ssr2.getLeft50() + "," + ssr2.getRight50() + '\n')
+	outFile.close()
+
 
